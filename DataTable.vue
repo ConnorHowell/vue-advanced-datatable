c644d0266eff1cc7e22c3009623fb77de430d679
Connor Howell <connor.howell@mail.com>
2021-09-14 14:19:27 +0100

    Advanced filtering & persistent settings

--- DataTable.vue
+++ DataTable.vue
@@ -27,13 +27,17 @@
                 </div>
 
                 <div class="mr-5">
-                    <button class="btn btn-info" data-toggle="click-ripple" @click="toggleFilterRow()"><i class="fas fa-filter mr-5"></i> Filter Data</button>
+                    <button class="btn btn-info" data-toggle="click-ripple" v-b-modal="`modal-datatable-filter-${_uid}`"><i class="fas fa-filter mr-5"></i> Filter Data</button>
+                </div>
+
+                <div class="mr-5">
+                    <button class="btn btn-secondary" data-toggle="click-ripple" @click="toggleTableView()"><i class="fas fa-desktop mr-5"></i> Toggle View</button>
                 </div>
 
                 <div class="flex flex-col items-center">
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" @click="showColumnDropdown = !showColumnDropdown">
-                            Show / Hide Columns
+                            Show / Hide Columns <span class="font-w700" v-if="hiddenFields.length > 0">({{ immutableColumns.length - hiddenFields.length }}/{{ immutableColumns.length }})</span>
                         </button>
                         <div :class="{'dropdown-menu': true, show: showColumnDropdown}">
                             <button class="dropdown-item" type="button" @click="toggleColumn(col)" v-for="col in immutableColumns" :key="col">
@@ -46,7 +50,7 @@
             </div>
         </div>
         <div :class="{'table-responsive': responsive, 'border': bordered, 'wrapper': stickyHeader}">
-            <table :class="tableClass" id="table-output">
+            <table :class="tableClass" id="report-output">
                 <thead :class="theadClass">
                     <tr>
                         <th :data-header="col" v-for="col in columns" :key="col" @click="toggleSort(col)" class="shadow-sm">
@@ -57,28 +61,14 @@
                         </th>
                     </tr>
                 </thead>
-                <tbody v-if="results.length && filterRow">
-                    <tr class="table-info">
-                        <td v-for="col in columns" :key="col">
-                            <div class="input-group">
-                                <input type="text" class="form-control" v-model.lazy="filterValues[col]" :placeholder="`Filter ${col}...`">
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn btn-secondary" @click="filterValues[col] = ''">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
                 <tbody>
-                    <tr v-show="!results.length && showEmpty">
+                    <tr v-show="!results.length">
                         <td :colspan="columns.length" class="text-center text-muted">
                             <em>There is no data to display...</em>
                         </td>
                     </tr>
-                    <tr v-for="(row, key) in results" :key="key" :style="getRowStyling(row)">
-                        <td :data-column="col" v-for="col in columns" :key="col" :style="getColumnStyling(row, col)">{{ row[col] }}</td>
+                    <tr v-for="(row, key) in results" :key="key" :style="getRowStyling(row)" :class="{'expanded': expandTable}">
+                        <td :data-column="col" v-for="col in columns" :key="col" :style="getColumnStyling(row, col)" v-html="displayValue(row[col], col)"></td>
                     </tr>
                 </tbody>
                 <tfoot v-show="totals" style="position: sticky;bottom: -1px;">
@@ -88,6 +78,60 @@
                 </tfoot>
             </table>
         </div>
+        <b-modal :id="`modal-datatable-filter-${_uid}`" scrollable title="Filter Table" size="lg">
+            <template #default>
+                <div class="form-group" v-for="col in columns" :key="col">
+                    <label :for="`filter-${col}`">{{ col | heading }}</label>
+
+                    <div class="input-group" v-if="getFilterOptions(col)">
+                        <vue-select v-model.lazy="filterValues[col]" multiple>
+                            <option :value="opt" v-for="opt in getFilterOptions(col)" :key="opt">{{ opt }}</option>
+                        </vue-select>
+                    </div>
+
+                    <div class="input-group" v-else-if="getFieldType(col) == 'Boolean'">
+                        <b-form-checkbox v-model="filterValues[col]" switch size="lg">
+                            {{ col | heading }}
+                        </b-form-checkbox>
+                    </div>
+
+                    <div class="input-group" v-else-if="getFieldType(col) == 'Date'">
+                        <date-picker mode="range" format="Y-m-d" v-model.lazy="filterValues[col]"></date-picker>
+                    </div>
+
+                    <template v-else-if="getFieldType(col) == 'Number'">
+                        <div class="input-group mb-5">
+                            <input type="number" class="form-control" v-model.lazy="filterValues[col].min" placeholder="Min">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fas fa-angle-right"></i></span>
+                            </div>
+                            <input type="number" class="form-control" v-model.lazy="filterValues[col].max" placeholder="Max">
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-secondary" @click="filterValues[col] = {min: null, max: null}">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </template>
+                    <div class="input-group" v-else>
+                        <input type="text" class="form-control" v-model.lazy="filterValues[col]" :placeholder="`Filter ${col}...`">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-secondary" @click="filterValues[col] = ''">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <template #modal-footer="{ ok }">
+                <b-button variant="info" @click="clearFilters()">
+                    Clear Filters
+                </b-button>
+                <b-button variant="success" @click="ok()">
+                    OK
+                </b-button>
+            </template>
+        </b-modal>
     </div>
 </template>
 
@@ -110,18 +154,57 @@
                 hiddenFields: [],
                 filterValues: {},
                 showColumnDropdown: false,
-                filterRow: false,
-                hideTotals: false
+                expandTable: true,
+                hideTotals: false,
+                fieldTypes: {
+                    "INT1": "Number",
+                    "INTN": "Number",
+                    "BIT": "Boolean",
+                    "INT2": "Number",
+                    "INT4": "Number",
+                    "DATETIM4": "Date",
+                    "FLT4": "Number",
+                    "FLTN": "Number",
+                    "MONEY": "Number",
+                    "DATETIME": "Date",
+                    "DATETIMN": "Date",
+                    "FLT8": "Number",
+                    "DECIMAL": "Number",
+                    "NUMERIC": "Number",
+                    "MONEY4": "Number",
+                    "INT8": "String",
+                    "IMAGE": "String",
+                    "TEXT": "String",
+                    "GUIDN": "String",
+                    "NTEXT": "String",
+                    "BIGVARBIN": "String",
+                    "BIGVARCHR": "String",
+                    "BIGBinary": "String",
+                    "BIGCHAR": "String",
+                    "NVARCHAR": "String",
+                    "NCHAR": "String",
+                    "XML": "String",
+                    "TIMEN": "Date",
+                    "DATEN": "Date",
+                    "DATETIME2N": "Date",
+                    "DATETIMEOFFSETN": "Date",
+                    "UDTTYPE": "String",
+                    "SSVARIANTTYPE": "String"
+                }
             }
         },
         props: {
             data: Array,
             tableClass: {
                 type: String,
-                default: 'table table-hover border',
+                default: 'table table-hover table-vcenter border',
                 required: false
             },
             theadClass: {
+                type: String,
+                required: false
+            },
+            identifier: {
                 type: String,
                 required: false
             },
@@ -133,6 +216,13 @@
             conditionalFormatting: {
                 type: Array,
                 required: false
+            },
+            reportMeta: {
+                type: Array,
+                required: false,
+                default: () => {
+                    return [];
+                }
             },
             bordered: {
                 type: Boolean,
@@ -166,12 +256,25 @@
                 val.forEach(column => {
                     this.$set(this.filterValues, column, '');
                 })
+            },
+            reportMeta: {
+                immediate: true,
+                handler(val) {
+                    this.filterValues = {};
+                    val.forEach(column => {
+                        if(this.getFieldType(column.colName) == 'Number') {
+                            this.$set(this.filterValues, column.colName, {min: null, max: null});
+                        } else this.$set(this.filterValues, column.colName, '');
+                })
+                }
             }
         },
         computed: {
             results() {
                 this.$nextTick(() => {
-                    this.busy = false;
+                    setTimeout(() => {
+                        this.busy = false;
+                    }, 250);
                 })
                 if(this.searchTerm) return this.filteredData.filter(row => {
                     let values = Object.values(row);
@@ -182,18 +285,38 @@
             filteredData() {
                 return this.data.filter(row => {
                     return this.columns.every(item => {
-                        if(!this.filterValues[item]) return true;
-                        return (String(row[item]).indexOf(this.filterValues[item]) > -1);
+                        if(this.filterValues[item] === '' || this.filterValues[item] == null || ((Array.isArray(this.filterValues[item]) && this.filterValues[item].length < 1))) return true; //If no filters exist
+
+                        if(this.getFieldType(item) == 'Date') {
+                            if(this.filterValues[item].includes('to')) {
+                                let dateParts = this.filterValues[item].split(' to ').map(v => Date.parse(v));
+                                return (Date.parse(row[item]) <= dateParts[1] && Date.parse(row[item]) >= dateParts[0]);
+                            } else return (String(row[item]).indexOf(this.filterValues[item]) > -1);
+                        }
+
+                        if(this.getFieldType(item) == 'Number') {
+                            let range = this.filterValues[item];
+                            if(range.min == null && range.max == null) return true;
+                            return (Number(row[item]) >= (range.min || 0) && Number(row[item]) <= (range.max || 999999));
+                        }
+
+                        if(Array.isArray(this.filterValues[item])) return (this.filterValues[item].includes(row[item])); // If multi select dropdown
+
+                        return (String(row[item]).indexOf(this.filterValues[item]) > -1); // If text input
                     })
                 })
             },
             immutableColumns() {
                 if(!this.data || !this.data.length) return [];
-                return Object.keys(this.data[0]);
+                if(this.reportMeta.length > 0) {
+                    return this.reportMeta.map(v => v.colName);
+                } else return Object.keys(this.data[0]);
             },
             columns() {
                 if(!this.data || !this.data.length) return [];
-                return Object.keys(this.data[0]).filter(v => (!this.hiddenFields.includes(v)));
+                if(this.reportMeta.length > 0) {
+                    return this.reportMeta.map(v => v.colName).filter(v => (!this.hiddenFields.includes(v)));
+                } else return Object.keys(this.data[0]).filter(v => (!this.hiddenFields.includes(v)));
             }
         },
         filters: {
@@ -201,7 +324,50 @@
                 return val.replace(/_/g, ' ');
             },
         },
+        mounted() {
+            // Load config from storage
+            if(localStorage.useCondensedReportView != null) {
+                this.expandTable = !(localStorage.useCondensedReportView === 'true');
+            }
+
+            if(this.identifier && localStorage[`table_${this.identifier}_hidden_columns`] != null) {
+                this.hiddenFields = localStorage[`table_${this.identifier}_hidden_columns`].split(',');
+            }
+        },
         methods: {
+            toggleTableView() {
+                this.expandTable = !this.expandTable;
+                localStorage.useCondensedReportView = !this.expandTable;
+            },
+            clearFilters() {
+                Object.keys(this.filterValues).forEach((key) => {
+                    if(this.getFieldType(key) == 'Number') {
+                        this.filterValues[key] = {min: null, max: null};
+                    } else this.filterValues[key] = '';
+                });
+            },
+            getFilterOptions(col_name) {
+                let type = this.getFieldType(col_name);
+                if(['Boolean', 'Number', 'Date'].includes(type)) return null;
+                let options = [...new Set(this.data.map(item => item[col_name]))];
+                if(!options.length || options.length > 500) return null; // Revert to text field as too many options
+
+                return options.sort();
+            },
+            displayValue(value, col_name) {
+                let type = this.getFieldType(col_name);
+                switch (type) {
+                    case 'Boolean':
+                        return (value ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>');
+                    default:
+                        return value;
+                }
+            },
+            getFieldType(col) {
+                if(!this.reportMeta || !this.reportMeta.length) return 'String';
+                let type = this.reportMeta.find(v => (v.colName == col)).type.type;
+                return (this.fieldTypes[type]) ? this.fieldTypes[type] : 'String';
+            },
             getRowStyling(row) {
                 if(!this.conditionalFormatting) return;
                 let conditions = this.conditionalFormatting.filter(v => (v.transformation == 'row-colour'));
@@ -266,15 +432,12 @@
                 this.hideTotals = true;
                 this.busy = true;
                 setTimeout(() => {
-                    var elt = document.getElementById('table-output');
+                    var elt = document.getElementById('report-output');
                     var book = XLSX.utils.table_to_book(elt, {sheet:"Table Output", dateNF:'dd/mm/yyyy h:mm:ss', cellDates: true, display: true });
-                    XLSX.writeFile(book, `table-output_${Date.now()}.xlsx`)
+                    XLSX.writeFile(book, `report-output_${Date.now()}.xlsx`)
                     this.busy = false;
                     this.hideTotals = false;
                 }, 250);
-            },
-            toggleFilterRow() {
-                this.filterRow = !this.filterRow;
             },
             toggleColumn(col) {
                 if(this.hiddenFields.includes(col)) {
@@ -285,6 +448,9 @@
                     return;
                 }
                 this.hiddenFields.push(col);
+                if(this.identifier) {
+                    localStorage[`table_${this.identifier}_hidden_columns`] = this.hiddenFields;
+                }
             },
             sortTable() {
                 this.data = this.data.sort((a,b) => (
@@ -318,6 +484,10 @@
         width: 100%;
         max-height: 80vh;
         overflow-y: auto;
+    }
+
+    .expanded td {
+        white-space: nowrap !important;
     }
 
     thead tr th {
